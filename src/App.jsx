d7b6f1d9cbf98b0019dcfe7574770f1392bc9662
Kiran Adari <adarikiran1@gmail.com>
2024-09-27 02:22:57 +0530

    making components re-usable with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import reactImage from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import propsImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
@@ -21,10 +25,48 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
+      {/*This is css id for styling and it was already present in css file*/}
+      <div>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="Props"
+              description="Make components configurable (and therefore reusable) by passing input data to them."
+              image={propsImg}
+            />
+            <CoreConcept
+              title="JSX"
+              description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will ve rendered."
+              image={jsxImg}
+            />
+            <CoreConcept
+              title="State"
+              description="React-managed data which, when changed. causes the component to re-render & the UI to update."
+              image={stateImg}
+            />
+          </ul>
+        </section>
+      </div>
       <main>
         <h2>Time to get started!</h2>
       </main>
